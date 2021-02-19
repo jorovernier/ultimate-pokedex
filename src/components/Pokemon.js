b50ba4d1f79c45fcdc20283f57d6bcfee28c6a78
jorovernier <55214971+jorovernier@users.noreply.github.com>
2021-02-18 22:20:17 -0700

    updated axios and fixed formdex

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -40,7 +40,7 @@ export default class Pokemon extends Component {
             <div className='pokemon-box'>
                 <div className='pokemon'>
                     <button className='sprite' onClick={() => {this.props.action(pokemon.name)}}>
-                        <img src={forPMDB === 'floette-eternal' ? 'https://cdn.bulbagarden.net/upload/0/05/670EMS.png' : `https://img.pokemondb.net/sprites/sun-moon/icon/${forPMDB}.png`} alt={`sprite of ${forPMDB}`} />
+                        <img src={forPMDB === 'floette-eternal' ? 'https://img.pokemondb.net/sprites/sun-moon/icon/floette.png' : `https://img.pokemondb.net/sprites/sun-moon/icon/${forPMDB}.png`} alt={`sprite of ${forPMDB}`} />
                     </button>
                     <div className='name'> {this.prettify(species)} </div>
                 </div>
